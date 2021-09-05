dbd6b927f5e5521337f7c49cf098f6246d60976d
egighaniyyu <egidgm123@gmail.com>
2021-09-05 14:49:51 +0800

    explore vue js

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header />
+    <CarouselShayna />
+    <CarouselProduct />
+    <InstaShayna />
+    <LogoPartner />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import CarouselShayna from "@/components/CarouselShayna.vue";
+import CarouselProduct from "@/components/CarouselProduct.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import LogoPartner from "@/components/LogoPartner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    CarouselShayna,
+    CarouselProduct,
+    InstaShayna,
+    LogoPartner,
+    Footer,
+  },
+};
+</script>
